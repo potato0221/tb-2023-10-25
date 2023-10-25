dbeee6f5993090ffdc3ee93b09b146ee41a73b19
potato0221 <wnqo10@naver.com>
2023-10-25 15:36:47 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 class App {
     void run() {
-        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
+            System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+            if (cmd.equals("종료")){
+                break;
+            }
+            System.out.printf("입력하신 명령 : %s\n", cmd);
 
-        System.out.printf("입력하신 명령 : %s\n", cmd);
-
+        }
     }
 }
