e2d158ec43ec47903070da1ff2ca3aafd9d0dd8f
potato0221 <wnqo10@naver.com>
2023-10-31 10:50:23 +0900

    Create studytest.java

---
+++ YYJ/studytest.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        for(int i=0;i<my_string.length;i++){
+            
+            
+        }
+        return answer;
+    }
+}
